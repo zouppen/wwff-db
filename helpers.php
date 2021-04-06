c800038cfd321d2b6da064b7d4fd4a4aca95b15d
Joel Lehtonen <joel.lehtonen@iki.fi>
2021-04-07 00:17:11 +0200

    Add support for warnx in addition to errx

--- helpers.php
+++ helpers.php
@@ -2,6 +2,11 @@
 
 // Behaves like errx(3) from glibc and BSD
 function errx($exit, $msg, ...$va) {
-    error_log(vsprintf($msg, $va));
+    warnx($msg, ...$va);
     exit($exit);
+}
+
+// Behaves like warnx(3) from glibc and BSD
+function warnx($msg, ...$va) {
+    error_log(vsprintf($msg, $va));
 }
