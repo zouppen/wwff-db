61d3003b1802e7836d26b82001993549bb695b33
Joel Lehtonen <joel.lehtonen@iki.fi>
2021-04-06 02:06:28 +0200

    Populate park data to JSON, update if exists

--- schema.sql
+++ schema.sql
@@ -18,4 +18,15 @@ CREATE SEQUENCE public.wwff_id_seq
     NO MAXVALUE
     CACHE 1;
 
+ALTER TABLE ONLY public.wwff ALTER COLUMN id SET DEFAULT nextval('public.wwff_id_seq'::regclass);
+
+ALTER TABLE ONLY public.wwff ADD CONSTRAINT wwff_pkey PRIMARY KEY (id);
+
 CREATE UNIQUE INDEX wwff_ts_park_local_remote_frequency_idx ON public.wwff USING btree (ts, park, local, remote, frequency);
+
+CREATE TABLE public.wwff_park (
+    park text NOT NULL,
+    info jsonb NOT NULL
+);
+
+ALTER TABLE ONLY public.wwff_park ADD CONSTRAINT wwff_park_pkey PRIMARY KEY (park);
